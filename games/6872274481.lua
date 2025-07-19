7f3b1eb7c8608dac536e55fc88376bcd955d9f9f
CreamyWare <svedonikadrian@gmail.com>
2025-07-19 23:10:46 +0200

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -12282,7 +12282,7 @@ run(function()
 		List = WinEffectName
 	})
 end)
-	
+
 run(function()
 	local damageboost = nil
 	local damageboostduration = nil
@@ -12308,13 +12308,17 @@ run(function()
 						if moveDirection and moveDirection.Magnitude > 0 then
 							speedEnd = tick() + damageboostduration.Value
 							damageMultiplier = damageboostmultiplier.Value
-							if vape.Modules["Speed"].Enabled and rawget(_G, "speedLogicEnabled") then
+							if vape.Modules["Speed"].Enabled then
 								speedWasEnabled = true
-								_G.speedLogicEnabled = false
+								vape.Modules["Speed"]:Toggle()
+							else
+								speedWasEnabled = false
 							end
-							if vape.Modules["Invisibility"].Enabled then
+							if vape.Modules["Invisibility"] and vape.Modules["Invisibility"].Enabled then
 								invisWasEnabled = true
 								vape.Modules["Invisibility"]:Toggle()
+							else
+								invisWasEnabled = false
 							end
 						end
 					end
@@ -12327,13 +12331,13 @@ run(function()
 						local hrp = char and char:FindFirstChild("HumanoidRootPart")
 						local hum = char and char:FindFirstChild("Humanoid")
 						if hrp and hum and hum.MoveDirection.Magnitude > 0 then
-							local direction = hrp.CFrame.LookVector * damageMultiplier * 25
+							local direction = hum.MoveDirection.Unit * damageMultiplier * 25
 							hrp.Velocity = Vector3.new(direction.X, hrp.Velocity.Y, direction.Z)
 						end
 					elseif not restoring then
 						restoring = true
 						if speedWasEnabled then
-							_G.speedLogicEnabled = true
+							vape.Modules["Speed"]:Toggle()
 							speedWasEnabled = false
 						end
 						if invisWasEnabled then
@@ -12351,7 +12355,7 @@ run(function()
 					connection = nil
 				end
 				if speedWasEnabled then
-					_G.speedLogicEnabled = true
+					vape.Modules["Speed"]:Toggle()
 					speedWasEnabled = false
 				end
 				if invisWasEnabled then
@@ -12377,4 +12381,4 @@ run(function()
 		Decimal = 20,
 		Default = 1.4
 	})
-end)
+end)																																																																																																																																																																																									
