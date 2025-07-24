8c8553d9479b4bf8eca3e60d30150ae2d117de4c
CreamyWare <svedonikadrian@gmail.com>
2025-07-24 21:59:53 +0200

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -53,7 +53,7 @@ local tween = {
 local uipallet = {
 	Main = Color3.fromRGB(0, 0, 0),
 	Text = Color3.fromRGB(255, 255, 255),
-	Font = Font.fromEnum(Enum.Font.Arial),
+	Font = Font.fromEnum(Enum.Font.gotham),
 	FontSemiBold = Font.fromEnum(Enum.Font.Arial, Enum.FontWeight.SemiBold),
 	Tween = TweenInfo.new(0.16, Enum.EasingStyle.Linear)
 }
@@ -314,7 +314,7 @@ local function downloadFile(path, func)
 	if not isfile(path) then
 		createDownloader(path)
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/CreamyWare/d/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
